620d941c380925e9495c00ea5bd8a4c50e779d19
Luis <lvas248@gmail.com>
2023-01-13 15:55:27 -0500

    set funcitonality to keep user logged in on refresh in the backend: set route, update user controller by adding show action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,20 @@
 class SessionsController < ApplicationController
 
-  def create
-    user = User.find_by_username(params[:username])
-    if user&.authenticate(params[:password])
-      session[:user_id] = user.id
-      render json: user, status: :created
-    else
-      render json: { error: 'Invalid Username or Password'}, status: :unprocessable_entity
+    #Login user
+    def create
+      user = User.find_by_username(params[:username])
+      if user&.authenticate(params[:password])
+        session[:user_id] = user.id
+        render json: user, status: :created
+      else
+        render json: { error: 'Invalid Username or Password'}, status: :unprocessable_entity
+      end
     end
-  end
 
-  def destroy
-      session.delete :user_id
-      head :no_content
-  end
-
-  
-
+    #Logout user
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
 
-
-
+    #new user 
     def create
         user = User.create!(user_params)
         session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    #page refresh/ keep user signed in
+    def show
+        user = User.find(session[:user_id])
         render json: user, status: :created
     end
 
@@ -14,6 +19,5 @@ class UsersController < ApplicationController
     def user_params
         params.permit(:username, :password, :password_confirmation)
     end
-
 
 end
